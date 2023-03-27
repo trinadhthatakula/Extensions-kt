53b75e923003d9ede2ca71f450755300bbdfc43b
Trinadh Thatakula <33689525+trinadhthatakula@users.noreply.github.com>
2023-03-27 09:54:44 +0530

    Compose Extensions
    Compose functions to make developing in compose much more easy

---
+++ ComposeX.kt
@@ -0,0 +1,28 @@
+package com.rstech.camerascanner.pdfscanner.camscanner.easy.scan.camscanner.model
+
+import androidx.annotation.RawRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import com.airbnb.lottie.compose.*
+
+///Compose function to animate Lottie Raw file easily
+@Composable
+fun AnimateLottieRaw(
+    modifier: Modifier = Modifier,
+    @RawRes resId: Int,
+    shouldLoop: Boolean = false,
+    repeatCount: Int = LottieConstants.IterateForever
+) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(resId))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = if (shouldLoop) repeatCount else 1
+    )
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = modifier,
+    )
+}
